668e744c78853a6c36f1ecb1ba6a093380df880d
Didi Kusnadi <didi.kusnadi@electgo.com>
2025-11-06 14:24:55 +0700

    init enum value

---
+++ index.js
@@ -0,0 +1,10 @@
+export enum CartStatusEnum {
+  inactive = "inactive",
+  active = "active",
+  ordered = "ordered",
+  quote_new = "quote_new",
+  quote_submitted = "quote_submited",
+  quote_modified = "quote_modified",
+  quote_ordered = "quote_ordered",
+  quote_expired = "quote_expired"
+}
