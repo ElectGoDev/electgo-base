f6b9df2b12edb40eba34677c12eacd0757bac2b4
Didi Kusnadi <didi.kusnadi@electgo.com>
2025-11-10 20:12:38 +0700

    [fixing] enum format

--- src/enum.ts
+++ src/enum.ts
@@ -1,28 +1,28 @@
-export enum CartStatusEnum {
-  inactive = "inactive",
-  active = "active",
-  ordered = "ordered",
-  quote_new = "quote_new",
-  quote_submitted = "quote_submited",
-  quote_modified = "quote_modified",
-  quote_ordered = "quote_ordered",
-  quote_expired = "quote_expired"
+export const CartStatusEnum = {
+  inactive: "inactive",
+  active: "active",
+  ordered: "ordered",
+  quote_new: "quote_new",
+  quote_submitted: "quote_submited",
+  quote_modified: "quote_modified",
+  quote_ordered: "quote_ordered",
+  quote_expired: "quote_expired"
 }
 
-export enum CurrencyEnum {
-  sgd = "sgd",
-  idr = "idr",
-  usd = "usd",
+export const CurrencyEnum = {
+  sgd: "sgd",
+  idr: "idr",
+  usd: "usd",
 }
 
-export enum LocaleEnum {
-  en = "en",
-  id = "id",
+export const LocaleEnum = {
+  en: "en",
+  id: "id",
 }
 
-export enum FieldTypeEnum {
-  text = "text",
-  textarea = "textarea",
-  dropdown = "dropdown",
-  password = "password"
+export const FieldTypeEnum = {
+  text: "text",
+  textarea: "textarea",
+  dropdown: "dropdown",
+  password: "password"
 }
